be5e9b1587d7fca03043b186fdeef4293b5a8267
Ankit Chemjong <npp.rasik@gmail.com>
2024-09-09 21:28:35 +0545

    foods

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1> Welcome to recipe app</h1>
+      <h1> Welcome to recipe app of mine</h1>
       <Link href={'/recipe-list'}>Explore Recipes and Foods</Link>
     </div>
   );
